5d147005a356a3d7a727146e3c71b9fee3b8869d
mochenya <mochen20070702@gmail.com>
2026-01-03 18:15:25 +0800

    🔧 chore: 完善包重定向配置，添加 models 和 core 模块的 __init__.py 导出

---
+++ src/ecjtu_wechat_api/models/__init__.py
@@ -0,0 +1,19 @@
+from ecjtu_wechat_api.models.course import (
+    Course,
+    CourseSchedule,
+    DateInfo,
+)
+from ecjtu_wechat_api.models.score import (
+    ScoreItem,
+    StudentScoreInfo,
+    TermItem,
+)
+
+__all__ = [
+    "ScoreItem",
+    "StudentScoreInfo",
+    "TermItem",
+    "Course",
+    "CourseSchedule",
+    "DateInfo",
+]
